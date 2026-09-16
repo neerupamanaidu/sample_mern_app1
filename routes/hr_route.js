ef3201dbf6e452a8980475984d83029d196a737e
neerupamanaidu <neerupamanaidu@gmail.com>
2026-09-16 12:43:44 +0530

    frst_commit

---
+++ routes/hr_route.js
@@ -0,0 +1,12 @@
+let express=require("express");
+let router=express.Router();
+router.get("/viewemployees",(req,res)=>{
+    res.send("View Employees router");
+});
+router.post("/assignemployees",(req,res)=>{
+    res.send("Assign Employees router");
+})
+router.delete("/deleteemployees",(req,res)=>{
+    res.send("Delete Employees router");
+})
+module.exports=routes
