f128931320486ccfb76d1197d82f965978c61bd3
neerupamanaidu <neerupamanaidu@gmail.com>
2026-09-24 10:46:18 +0530

    fifth commit

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ mongoose.connect("mongodb://127.0.0.1:27017/hrmanagement").then(()=>{
 app.use(express.json());
 app.use("/api/hr",hrroutes);
 app.use("/api/emp",emproutes);
-
 app.listen(3000,()=>{
     console.log("server listening on port 3000")
 })
--- routes/hr_route.js
+++ routes/hr_route.js
@@ -1,20 +1,22 @@
 let express=require("express");
 let router=express.Router();
-let{users}=require('../models/users')
-router.get("/viewemployees",async(req,res)=>{
-    let result=await users.find();
+let {users}=require("../models/users");
+let {tasks}=require("../models/tasks");
+router.get("/viewemployees",(req,res)=>{
+    res.send("View Employees router");
+});
+
+router.post("/assign-task",async(req,res)=>{
+    let data =req.body;
+    let newTask=new tasks(data);
+    let result=await newTask.save();
     res.send(result);
 });
+
 router.post("/assignemployees",(req,res)=>{
     res.send("Assign Employees router");
 })
-router.delete("/deleteemployee/:id",async(req,res)=>{
-    let result=await users.findByIdAndDelete(req.params.id)
-    if(result){
-    res.send("employee deleted success ");
-    }else{
-        res.send("no user found");
-    }
+router.delete("/deleteemployees",async (req,res)=>{
+    res.send("Delete Employees router");
 })
-
-module.exports=router;
+module.exports=router
